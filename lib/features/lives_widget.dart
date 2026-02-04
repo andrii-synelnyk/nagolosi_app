c64f458a9223a4c09676e673c866d1fe9ddd29d0
andrii-synelnyk <sineln.dev@gmail.com>
2026-02-04 11:25:23 +0100

    Make lives widget animated

--- lib/features/lives_widget.dart
+++ lib/features/lives_widget.dart
@@ -1,31 +1,43 @@
 import 'package:flutter/material.dart';
 
 class LivesWidget extends StatelessWidget {
-  const LivesWidget({
-    super.key,
-    required this.startLives,
-    required this.lives,
-  });
+  const LivesWidget({super.key, required this.startLives, required this.lives});
 
   final int startLives;
   final int lives;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        for (var i = 0; i < startLives; i++)
-          // Expanded(
-          //   child: LinearProgressIndicator(
-          //     value: lives <= i ? 0 : 1,
-          //     borderRadius: BorderRadius.circular(8),
-          //     color: Color(0xFFFD3B31), // Color(0xFFFD3B31) // Color(0xFFFF7070)
-          //     backgroundColor: Color(0xFF2A2A2A),
-          //     minHeight: double.infinity,
-          //   ),
-          // ),
-          Icon(Icons.favorite_rounded, size: 28, color: lives <= i ? Theme.of(context).colorScheme.secondaryContainer : Color(0xFFFF4B5D))
+    return Stack(
+      children: <Widget>[
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            for (var i = 0; i < startLives; i++)
+              Icon(
+                Icons.favorite_rounded,
+                size: 28,
+                color: Theme.of(context).colorScheme.secondaryContainer,
+              ),
+          ],
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            for (var i = 0; i < startLives; i++)
+              TweenAnimationBuilder(
+                tween: Tween<double>(end: lives <= i ? 0 : 1),
+                duration: const Duration(milliseconds: 300),
+                builder: (context, value, child) {
+                  return Icon(
+                    Icons.favorite_rounded,
+                    size: 28,
+                    color: Color(0xFFFF4B5D).withValues(alpha: value),
+                  );
+                },
+              ),
+          ],
+        ),
       ],
     );
   }
