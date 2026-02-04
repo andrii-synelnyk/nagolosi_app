4238de205d57c7ffce99b07f53354b1d67ce4e1c
andrii-synelnyk <sineln.dev@gmail.com>
2026-02-04 17:03:42 +0100

    Improve dictionary screen

--- lib/features/dictionary/dictionary_screen.dart
+++ lib/features/dictionary/dictionary_screen.dart
@@ -26,27 +26,50 @@ class DictionaryScreen extends StatelessWidget {
                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(20.0),
+                  padding: const EdgeInsets.all(16.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      for (int k = 0; k < controller.words[i].length; k++)
-                        Text(
-                          controller.words[i][k],
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Theme.of(context).colorScheme.onSurfaceVariant,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
+                      for (final s in controller.words[i])
+                        buildWordLine(context, s),
                     ],
                   ),
                 ),
-                SizedBox(height: 20),
+                SizedBox(height: 16),
               ],
             ],
           );
-        }
+        },
+      ),
+    );
+  }
+
+  Widget buildWordLine(BuildContext context, String raw) {
+    final parts = raw.replaceFirst(" ", "|").split("|");
+    final word = parts[0];
+    final desc = parts.length > 1 ? parts[1] : null;
+
+    return Text.rich(
+      TextSpan(
+        children: [
+          TextSpan(
+            text: word,
+            style: TextStyle(
+              fontSize: 18,
+              color: Theme.of(context).colorScheme.onSurfaceVariant,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          if (desc != null)
+            TextSpan(
+              text: " $desc",
+              style: TextStyle(
+                fontSize: 18,
+                color: Theme.of(context).colorScheme.onSurface,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+        ],
       ),
     );
   }
